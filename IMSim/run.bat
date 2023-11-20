5b531503a994ba7653117d69fbfb56f3e68f99fc
mingf2 <mingf2@illinois.edu>
2023-11-20 12:34:04 -0600

    update exe path in bat and bash scripts

--- IMSim/run.bat
+++ IMSim/run.bat
@@ -1,20 +1,20 @@
 @echo off
 set sim_dir=C:\Users\ming\Documents\imagerSimPost\IMSim
-set exe_dir=C:\Users\ming\Documents\imagerSimPost\build\Sources
+set exe_dir=C:\Users\ming\Documents\imagerSimPost\build
 set collision_file_name=comptonImager0.d
 
 cd /d %sim_dir%
 
 echo "  sort by hist"
-"%exe_dir%\sortbyhist\Release\sortbyhist" %sim_dir%
+"%exe_dir%\Sources\sortbyhist\Release\sortbyhist" %sim_dir%
 
 echo "  find pos"
-"%exe_dir%\findposition\Release\findposition" %sim_dir% %collision_file_name%
+"%exe_dir%\Sources\findposition\Release\findposition" %sim_dir% %collision_file_name%
 
 echo "  sort by time"
-"%exe_dir%\sortbytime\Release\sortbytime" %sim_dir%
+"%exe_dir%\Sources\sortbytime\Release\sortbytime" %sim_dir%
 
 echo "  find coincidences"
-"%exe_dir%\findCoincidences\Release\findCoincidences" %sim_dir%
+"%exe_dir%\Sources\findCoincidences\Release\findCoincidences" %sim_dir%
 
 echo "Done."
--- IMSim/run.sh
+++ IMSim/run.sh
@@ -4,24 +4,24 @@
  # @Author: Ming Fang
  # @Date: 2021-10-14 17:37:33
  # @LastEditors: Ming Fang
- # @LastEditTime: 2023-05-10 18:09:58
+ # @LastEditTime: 2023-11-20 12:33:29
 ### 
 # i=1
 sim_dir=/media/ming/DATA/projects/Postprocessing/IMSim
-exe_dir=/media/ming/DATA/projects/Postprocessing/build/Sources
+exe_dir=/media/ming/DATA/projects/Postprocessing/build
 collision_file_name="comptonImager0.d"
 cd ${sim_dir}
 
 echo "  sort by hist"
-${exe_dir}/sortbyhist/sortbyhist ${sim_dir}
+${exe_dir}/Sources/sortbyhist/sortbyhist ${sim_dir}
 
 echo "  find pos"
-${exe_dir}/findposition/findposition ${sim_dir} ${collision_file_name}
+${exe_dir}/Sources/findposition/findposition ${sim_dir} ${collision_file_name}
 
 echo "  sort by time"
-${exe_dir}/sortbytime/sortbytime ${sim_dir}
+${exe_dir}/Sources/sortbytime/sortbytime ${sim_dir}
 
 echo "  find coincidences"
-${exe_dir}/findCoincidences/findCoincidences ${sim_dir}
+${exe_dir}/Sources/findCoincidences/findCoincidences ${sim_dir}
 
 echo "Done."
